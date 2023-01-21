2089163083bef1327ca9e5c749d55c15d822944e
Edison Silva Filho <edisonsilvaf@gmail.com>
2023-01-21 10:52:32 -0300

    Add files via upload

---
+++ update-void.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Atualizar o Sistema
+echo "Iniciando instalação de complementos do voidlinux."
+pause
+sudo xbps-install -Suv
+pause
+# Repositorios non-free
+sudo xbps-install -Rs void-repo-nonfree epson-inkjet-printer-escpr noto-fonts-emoji noto-fonts-ttf noto-fonts-ttf-extra 
+pause
+# Essenciais
+sudo xbps-install curl wget git xz unzip zip vim gptfdisk xtools mtools mlocate ntfs-3g fuse-exfat bash-completion linux-headers gtksourceview4 ffmpeg mesa-vdpau mesa-vaapi autoconf automake bison m4 make libtool flex meson ninja optipng sassc xorg conky yt-dlp youtube-dl python3-pip gdm gnome gnome-browser-connector gnome-connections fragments NetworkManager NetworkManager-openvpn NetworkManager-openconnect NetworkManager-vpnc NetworkManager-l2tp pulseaudio pulseaudio-utils pulsemixer alsa-plugins-pulseaudio bluez cups cups-pk-helper cups-filters tlp tlp-rdw powertop foomatic-db foomatic-db-engine libreoffice libreoffice-i18n-pt-BR firefox firefox-i18n-pt-BR  
+pause
+# Diretorios HOME
+sudo xbps-install -Rs xdg-desktop-portal xdg-desktop-portal-gtk xdg-user-dirs xdg-user-dirs-gtk xdg-utils
+pause
+echo "Instalações concluidas com exito!!!"
+
